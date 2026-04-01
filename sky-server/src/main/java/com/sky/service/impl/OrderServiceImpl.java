8c0ef52eea90bb385593196ccb9a050b68487f91
Azusaga <lloydfoger@gmail.com>
2026-04-01 19:50:42 +0800

    客户催单业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -555,4 +555,25 @@ public class OrderServiceImpl implements OrderService {
             throw new OrderBusinessException("超出配送范围");
         }
     }
+
+
+    /**
+     * 用户催单
+     * @param id
+     */
+    public void reminder(Long id) {
+        //根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+        //校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+        }
+        Map map = new HashMap();
+        map.put("type", 2); //1表示来单提醒,2为客户催单
+        map.put("orderId", ordersDB.getId());
+        map.put("content", "客户催单，订单号：" + ordersDB.getNumber());
+        String json = JSON.toJSONString(map);
+        //通过websocket向客户端浏览器推送消息type orderId content
+        webSocketServer.sendToAllClient(json);
+    }
 }
